48934574bfff0774d6ed006e627abe150d2bef89
ioannisa <ioannisanif@gmail.com>
2026-07-15 13:27:01 +0300

    Update library versions and clean up theme references

--- app/src/main/java/eu/anifantakis/networkapp/features/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/eu/anifantakis/networkapp/features/core/presentation/ui/theme/Theme.kt
@@ -11,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.Purple80,
-    secondary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.PurpleGrey80,
-    tertiary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.Pink80
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.Purple40,
-    secondary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.PurpleGrey40,
-    tertiary = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.Pink40
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -44,13 +44,13 @@ fun NetworkAppTheme(
         val context = LocalContext.current
         if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
       }
-      darkTheme -> _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.DarkColorScheme
-      else -> _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.LightColorScheme
+      darkTheme -> DarkColorScheme
+      else -> LightColorScheme
     }
 
     MaterialTheme(
       colorScheme = colorScheme,
-      typography = _root_ide_package_.eu.anifantakis.networkapp.features.core.presentation.ui.theme.Typography,
+      typography = Typography,
       content = content
     )
 }
